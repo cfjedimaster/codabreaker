c4617ba93a7e6b5426a962f8d3413c5c5cc20544
Raymond Camden <raymondcamden@gmail.com>
2019-01-03 15:05:47 -0600

    test3

---
+++ functions/test2.js
@@ -0,0 +1,13 @@
+// credit: https://github.com/netlify/functions/blob/master/src/lambda/hello_fetch.js#L1
+import fetch from "node-fetch";
+
+exports.handler = async (event, context, callback) => {
+	
+	return fetch('https://cat-fact.herokuapp.com/facts/random')
+	.then(res => res.json())
+	.then(res => ({
+		statusCode:200,
+		body:res
+	}));
+
+}

aa4e95cd59cefd677eac2fcd990fea927253ae9c
Raymond Camden <raymondcamden@gmail.com>
2019-01-08 16:16:08 -0600

    new functions right?

--- functions/test.js
+++ functions/test.js
@@ -3,13 +3,14 @@ exports.handler = function(event, context, callback) {
 	
 	let data = {
 		name:'ray',
-		foo:[1,2,4],
+		foo:[1,2,4,6],
 		time:Date.now()
 	};
 	
-	console.log('data is '+JSON.stringify(data));
+	console.log('!!data is '+JSON.stringify(data));
 
 	callback(null, {
+		statusCode:200,
 		body:JSON.stringify(data)
 	});
 
--- functions/test2.js
+++ functions/test2.js
@@ -1,8 +1,9 @@
 // credit: https://github.com/netlify/functions/blob/master/src/lambda/hello_fetch.js#L1
 //import fetch from "node-fetch";
 
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 
+/*
 exports.handler = async (event, context, callback) => {
 	console.log('here goes nothing');
 	return fetch('https://cat-fact.herokuapp.com/facts/random')
@@ -11,7 +12,22 @@ exports.handler = async (event, context, callback) => {
 		statusCode:200,
 		body:res
 	}));
-
 }
 
 
+*/
+
+const axios = require('axios');
+
+exports.handler = (event, context, callback) => {
+  axios.get('https://cat-fact.herokuapp.com/facts/random')
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res.data),
+      });
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};

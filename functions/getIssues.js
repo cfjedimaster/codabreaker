76c1726d81c68e60798a87886c13f430b8617223
Raymond Camden <raymondcamden@gmail.com>
2019-01-09 13:01:36 -0600

    add title

--- functions/getIssues.js
+++ functions/getIssues.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const apiRoot = 'https://us6.api.mailchimp.com/3.0/campaigns?list_id=d00ad8719a&fields=campaigns.long_archive_url,campaigns.send_time&status=sent';
+const apiRoot = 'https://us6.api.mailchimp.com/3.0/campaigns?list_id=d00ad8719a&fields=campaigns.long_archive_url,campaigns.send_time,campaigns.settings.title&status=sent';
 
 exports.handler = (event, context, callback) => {
 	axios({

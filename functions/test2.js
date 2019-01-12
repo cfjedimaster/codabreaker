c0312019ce145c3be0e2a0710e586b97f6fb7e05
Raymond Camden <raymondcamden@gmail.com>
2019-01-12 09:16:12 -0600

    more

--- functions/test2.js
+++ functions/test2.js
@@ -1,22 +1,3 @@
-// credit: https://github.com/netlify/functions/blob/master/src/lambda/hello_fetch.js#L1
-//import fetch from "node-fetch";
-
-//const fetch = require('node-fetch');
-
-/*
-exports.handler = async (event, context, callback) => {
-	console.log('here goes nothing');
-	return fetch('https://cat-fact.herokuapp.com/facts/random')
-	.then(res => res.json())
-	.then(res => ({
-		statusCode:200,
-		body:res
-	}));
-}
-
-
-*/
-
 const axios = require('axios');
 
 exports.handler = (event, context, callback) => {

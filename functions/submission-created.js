e8899507a9264400ab40edd597d33851d327ab7d
Raymond Camden <raymondcamden@gmail.com>
2019-01-12 09:29:00 -0600

    testing

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,6 +2,7 @@
 exports.handler = (event, context, callback) => {
 	console.log('submission created called');
 	console.log('context, right? '+JSON.stringify(context));
+	console.log('event, right? '+JSON.stringify(event));
 	
 	callback(null, {
 		body:'um'

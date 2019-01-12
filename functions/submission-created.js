4387299449566411b967ea4ced6a9b93ce101cdd
Raymond Camden <raymondcamden@gmail.com>
2019-01-12 10:02:19 -0600

    testing

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,23 +1,17 @@
 
 exports.handler = (event, context, callback) => {
-	console.log('submission created called um ray here');
-	//console.log('context, right? '+JSON.stringify(context));
-
-	//console.log('event, right? '+JSON.stringify(event.payload));
-	console.log('EVENT KEYS');
+	console.log('submission created error testing');
 	
 	let payload = JSON.parse(event.body).payload;
 	
-	console.log('ok i have payload');
-
 	console.log('testing payload '+JSON.stringify(payload));
-	
-	// so yes i have payload
 
-	//console.log(JSON.stringify(event.body));
-	console.log('END OF TEST');
-
+	/*
 	callback(null, {
 		body:'um'
 	});
+	*/
+
+	callback(new Error('Oops!'));
+	
 };

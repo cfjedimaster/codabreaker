c6b77b05d4bc65914cf490c758d404e618192ab0
Raymond Camden <raymondcamden@gmail.com>
2019-01-21 14:00:52 -0600

    testing random shit2

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 console.log('Running build script');
 
 const axios = require('axios');
+const fs = require('fs');
 
 const apiRoot = 'https://us6.api.mailchimp.com/3.0/campaigns?list_id=d00ad8719a&fields=campaigns.long_archive_url,campaigns.send_time,campaigns.settings.title&status=sent';
 
@@ -19,6 +20,8 @@ axios({
 	});
 	*/
 	console.log(JSON.stringify(res.data));
+	fs.writeFileSync('./static.json', JSON.stringify(res.data), 'UTF-8');
+
 })
 .catch(err => {
 	console.log('Error getting stuff', err);

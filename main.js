9e0c07320a3015518781a5b41089054c2810945a
Raymond Camden <raymondcamden@gmail.com>
2019-01-21 11:04:27 -0600

    morwe changes

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+
+const API = 'https://codabreaker.netlify.com/.netlify/functions/getIssues';
+
+Vue.filter('dateFm', d => {
+	return d.split('T')[0];
+});
+
 let app = new Vue({
-	
+	el:'#app',
+	data:{
+		issues:[]
+	},
+	created:function() {
+		fetch(API)
+		.then(res => res.json())
+		.then(res => {
+			this.issues = res.campaigns;
+		});
+	}
 });

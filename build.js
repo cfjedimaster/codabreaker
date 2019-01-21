fe41d30ac85b59c2068e503653abffefebd69fae
Raymond Camden <raymondcamden@gmail.com>
2019-01-21 13:58:30 -0600

    testing random shit

--- build.js
+++ build.js
@@ -1,2 +1,26 @@
-console.log('Running build');
-console.log('testing env '+process.env.MC_API);
+console.log('Running build script');
+
+const axios = require('axios');
+
+const apiRoot = 'https://us6.api.mailchimp.com/3.0/campaigns?list_id=d00ad8719a&fields=campaigns.long_archive_url,campaigns.send_time,campaigns.settings.title&status=sent';
+
+axios({
+	method:'get', 
+	url:apiRoot,
+	auth:{
+		'username':'anythingreally',
+		'password':process.env.MC_API
+	}
+}).then(res => {
+	/*
+	callback(null, {
+		statusCode: 200,
+		body: JSON.stringify(res.data)
+	});
+	*/
+	console.log(JSON.stringify(res.data));
+})
+.catch(err => {
+	console.log('Error getting stuff', err);
+});
+

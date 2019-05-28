f21c99265ba9c48cae02b97b3ff7a398fa2ec0c4
Raymond Camden <raymondcamden@gmail.com>
2019-05-28 16:12:35 -0500

    add sub ability

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
 
-const API = '//codabreaker.rocks/static.json';
+const ISSUES = './static.json';
+const SUBSCRIBE_API = '/.netlify/functions/newsletter-signup?email=';
 
 Vue.filter('dateFm', d => {
 	return d.split('T')[0];
@@ -10,13 +12,41 @@ Vue.filter('dateFm', d => {
 let app = new Vue({
 	el:'#app',
 	data:{
-		issues:[]
+		issues:[],
+		email:'',
+		status:''
 	},
 	created:function() {
-		fetch(API)
+		fetch(ISSUES)
 		.then(res => res.json())
 		.then(res => {
 			this.issues = res.campaigns;
+		})
+		.catch(e => {
+			// for now do nothing
 		});
+	},
+	methods:{
+		subscribe() {
+			if(this.email === '') return;
+			this.status = 'Attempting to subscribe...';
+
+			fetch(SUBSCRIBE_API + this.email)
+			.then(res => {
+				return res.json()
+			})
+			.then(res => {
+				
+				if(res.status === 'subscribed') {
+					this.status = 'Subscribed!';
+				} else if(res.status === 400) {
+					this.status = `There was an error: ${res.detail}`;
+				}
+				console.log('res', res);
+			})
+			.catch(e => {
+				console.log('error result', e);
+			});
+		}
 	}
 });

b13e736df3c2ffd8dbbc6c701b2d5116ece325a7
Raymond Camden <raymondcamden@gmail.com>
2019-07-12 15:41:02 -0500

    fix count on mailchimp

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ console.log('Running build script');
 const axios = require('axios');
 const fs = require('fs');
 
-const apiRoot = 'https://us6.api.mailchimp.com/3.0/campaigns?list_id=d00ad8719a&fields=campaigns.long_archive_url,campaigns.send_time,campaigns.settings.title&status=sent&sort_field=send_time&sort_dir=desc';
+const apiRoot = 'https://us6.api.mailchimp.com/3.0/campaigns?list_id=d00ad8719a&fields=campaigns.long_archive_url,campaigns.send_time,campaigns.settings.title&status=sent&sort_field=send_time&sort_dir=desc&count=1000';
 
 axios({
 	method:'get', 
@@ -24,6 +24,6 @@ axios({
 
 })
 .catch(err => {
-	console.log('Error getting stuff', err);
+	console.log('Error getting stuff', err.message);
 });
 
